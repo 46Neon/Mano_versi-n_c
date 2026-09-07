c25b247fd34dddce204db44e92327e580d7fc15d
46Neon <j7942281@gmail.com>
2026-09-06 23:59:07 -0400

    Begin forest classifier core

---
+++ include/forest.h
@@ -0,0 +1,31 @@
+#ifndef MILENA_FOREST_H
+#define MILENA_FOREST_H
+
+#include "array.h"
+
+typedef struct {
+    size_t feature;
+    double threshold;
+    int left_class;
+    int right_class;
+} MilenaDecisionStump;
+
+typedef struct {
+    size_t tree_count;
+    size_t feature_count;
+    MilenaDecisionStump *trees;
+} MilenaForestClassifier;
+
+void milena_forest_init(MilenaForestClassifier *forest);
+void milena_forest_release(MilenaForestClassifier *forest);
+MilenaStatus milena_forest_train(MilenaForestClassifier *forest,
+                                  const MilenaArray *features,
+                                  const MilenaArray *labels,
+                                  size_t tree_count,
+                                  MilenaError *error);
+MilenaStatus milena_forest_predict(const MilenaForestClassifier *forest,
+                                   const MilenaArray *features,
+                                   MilenaArray *predictions,
+                                   MilenaError *error);
+
+#endif
