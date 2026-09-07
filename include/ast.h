6b1ba79f8ac49ee67a32069a7f581562184d2d32
46Neon <j7942281@gmail.com>
2026-09-07 19:29:26 -0400

    Use repository headers for interpreter runtime API

--- include/ast.h
+++ include/ast.h
@@ -1,1 +1,83 @@
-#include "cur_include_ast.h"
+#ifndef MILENA_AST_H
+#define MILENA_AST_H
+
+#include "common.h"
+#include "token.h"
+
+typedef enum {
+    AST_PROGRAMA,
+    AST_BLOQUE_ANALISIS,
+    AST_DECLARACION_DATOS,
+    AST_DECLARACION_ESTADISTICA,
+    AST_ASIGNACION_DATASET,
+    AST_LLAMADA_CARGAR,
+    AST_BLOQUE_LIMPIAR,
+    AST_BLOQUE_TRANSFORMAR,
+    AST_BLOQUE_FILTRAR,
+    AST_BLOQUE_AGRUPAR,
+    AST_BLOQUE_RESUMIR,
+    AST_BLOQUE_VISUALIZAR,
+    AST_BLOQUE_EXPORTAR,
+    AST_EXPRESION_OPERACION,
+    AST_EXPRESION_LITERAL,
+    AST_EXPRESION_IDENTIFICADOR,
+    AST_EXPRESION_FUNCION,
+    AST_EXPRESION_ARRAY,
+    AST_EXPRESION_LLAMADA,
+    AST_BLOQUE_FUNCION,
+    AST_COMANDO_RETORNAR,
+    AST_CONDICION_SI,
+    AST_DECLARACION_FUNCION,
+    AST_DECLARACION_ARRAY,
+    AST_DECLARACION_VARIABLE,
+    AST_ASIGNACION_VARIABLE,
+    AST_COMANDO_NULOS,
+    AST_COMANDO_DUPLICADOS,
+    AST_COMANDO_CONDICION,
+    AST_COMANDO_EXTRAER,
+    AST_COMANDO_TOTAL,
+    AST_COMANDO_PERIODO,
+    AST_AGRUPACION_POR,
+    AST_RESUMEN_METRICA,
+    AST_OPERACION_ESTADISTICA
+} ASTNodeType;
+
+typedef enum {
+    AST_ESTADISTICA_NINGUNA,
+    AST_ESTADISTICA_SUMA,
+    AST_ESTADISTICA_MEDIA,
+    AST_ESTADISTICA_MINIMO,
+    AST_ESTADISTICA_MAXIMO,
+    AST_ESTADISTICA_VARIANZA,
+    AST_ESTADISTICA_DESVIACION,
+    AST_ESTADISTICA_MEDIANA,
+    AST_ESTADISTICA_PERCENTIL
+} ASTStatOperation;
+
+typedef struct ASTNode {
+    ASTNodeType type;
+    ASTStatOperation statistical_operation;
+    char *value;
+    double number_value;
+    double percentile;
+    int axis;
+    bool keepdims;
+    struct ASTNode **children;
+    size_t child_count;
+    size_t child_capacity;
+    int line;
+    int column;
+    struct ASTNode *parent;
+} ASTNode;
+
+ASTNode* ast_create(ASTNodeType type);
+ASTNode* ast_create_leaf(ASTNodeType type, const char *value);
+ASTNode* ast_create_number(double value);
+ASTNode* ast_create_statistic(ASTStatOperation operation, ASTNode *argument,
+                              int axis, bool keepdims, double percentile);
+void ast_add_child(ASTNode *parent, ASTNode *child);
+void ast_print(ASTNode *node, int depth);
+void ast_destroy(ASTNode *node);
+const char* ast_type_name(ASTNodeType type);
+
+#endif
