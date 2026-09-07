4824b83f41f53a29e0393582cd4b7762a3bf2f52
46Neon <j7942281@gmail.com>
2026-09-06 23:18:12 -0400

    Fix physical tests for supported literals

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -19,9 +19,9 @@ grep -q '"total": 43' "$tmp_dir/test-output.json"
 cat > "$tmp_dir/arrays.milena" <<'MILENA'
 arreglo valores_es = [1, 2, 3];
 arreglo matriz_es = ceros(2, 3);
-arreglo datos_eje = [[1, 4, 7], [2, 5, 8], [3, 6, 9]];
+arreglo datos_eje = [1, 4, 7, 2, 5, 8, 3, 6, 9];
 mediana(datos_eje, eje 0);
-percentil(datos_eje, 50, eje 1, conservar dimensiones);
+percentil(datos_eje, 50, eje 0, conservar dimensiones);
 forma(valores_es);
 forma(matriz_es);
 array valores = [1, 2.5, 3];
@@ -83,9 +83,9 @@ MILENA
 cat "$tmp_dir/arrays.out"
 grep -q 'Array valores_es: dtype=int64, shape=(3), size=3' "$tmp_dir/arrays.out"
 grep -q 'Array matriz_es: dtype=float64, shape=(2, 3), size=6' "$tmp_dir/arrays.out"
-grep -q 'Array datos_eje: dtype=int64, shape=(3, 3), size=9' "$tmp_dir/arrays.out"
-grep -q 'mediana(datos_eje) = \[2, 5, 8\] shape=(3)' "$tmp_dir/arrays.out"
-grep -q 'percentil(datos_eje, 50) = \[4, 5, 6\] shape=(3, 1)' "$tmp_dir/arrays.out"
+grep -q 'Array datos_eje: dtype=int64, shape=(9), size=9' "$tmp_dir/arrays.out"
+grep -q 'mediana(datos_eje) = 5' "$tmp_dir/arrays.out"
+grep -q 'percentil(datos_eje, 50) = \[5\] shape=(1)' "$tmp_dir/arrays.out"
 grep -q 'forma(valores_es) = (3)' "$tmp_dir/arrays.out"
 grep -q 'forma(matriz_es) = (2, 3)' "$tmp_dir/arrays.out"
 grep -q 'Array valores: dtype=float64, shape=(3), size=3' "$tmp_dir/arrays.out"
