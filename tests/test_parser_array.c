e9fd4e06a70f100b543f33a82bffbe9185a26b8f
46Neon <j7942281@gmail.com>
2026-09-07 23:27:52 -0400

    Add temporary diagnostic to parser array test

--- tests/test_parser_array.c
+++ tests/test_parser_array.c
@@ -1,6 +1,7 @@
 #include "parser.h"
 #include <assert.h>
 #include <string.h>
+#include <stdio.h>
 
 int main(void) {
     const char *source = "mediana(valores); percentil(valores, 90);";
@@ -10,6 +11,10 @@ int main(void) {
     parser_init(&parser, &lexer);
     ASTNode *program = parser_parse(&parser);
     assert(program);
+    if (parser.has_error) {
+        fprintf(stderr, "parser diagnostic: code=%d line=%zu column=%zu message=%s\n",
+                parser.error.code, parser.error.line, parser.error.column, parser.error.message);
+    }
     assert(!parser.has_error);
     assert(program->child_count == 2);
     assert(program->children[0]->type == AST_OPERACION_ESTADISTICA);
